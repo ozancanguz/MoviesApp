32152d4478464d09c5af32de54b8808cec3806ca
Ozan <ozancangu24@gmail.com>
2023-01-21 22:07:07 +0300

    -search by name details screen designed

--- app/src/main/java/com/ozancanguz/moviesapp/data/model/searchbyname/SearchNameResult.kt
+++ app/src/main/java/com/ozancanguz/moviesapp/data/model/searchbyname/SearchNameResult.kt
@@ -1,8 +1,11 @@
 package com.ozancanguz.moviesapp.data.model.searchbyname
 
 
+import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
+import kotlinx.android.parcel.Parcelize
 
+@Parcelize
 data class SearchNameResult(
     @SerializedName("imdbID")
     val imdbID: String,
@@ -14,4 +17,4 @@ data class SearchNameResult(
     val type: String,
     @SerializedName("Year")
     val year: String
-)
+):Parcelable
