d05589b4706a55b46e89fcb80a5ed04cef16dd2e
Ozan <ozancangu24@gmail.com>
2023-01-21 22:29:32 +0300

    -search film by id api function created

--- app/src/main/java/com/ozancanguz/moviesapp/data/api/MoviesApi.kt
+++ app/src/main/java/com/ozancanguz/moviesapp/data/api/MoviesApi.kt
@@ -1,5 +1,6 @@
 package com.ozancanguz.moviesapp.data.api
 
+import com.ozancanguz.moviesapp.data.model.searchbyid.SearchByIdModel
 import com.ozancanguz.moviesapp.data.model.searchbyname.SearchByName
 import retrofit2.Response
 import retrofit2.http.GET
@@ -15,6 +16,16 @@ interface MoviesApi {
     )
     @GET("imdbSearchByName")
     suspend fun searchFilmByName(@Query("query")query: String):Response<SearchByName>
+
+
+// search by imdb id
+    @Headers(
+        "authorization: apikey 2xAW88zA1iemGAxzWMNumI:47IyS3uICCasjqzWaFtxnn",
+        "content-type: application/json"
+    )
+    @GET("imdbSearchById")
+    suspend fun searchById(@Query("movieId")movieId:String):Response<SearchByIdModel>
+
 
 
 
