642a0e86a64fd75fbdc9ddf06c415e0bbe53f36e
Ozan <ozancangu24@gmail.com>
2023-01-21 17:32:29 +0300

    -search by name api function created

---
+++ app/src/main/java/com/ozancanguz/moviesapp/data/api/MoviesApi.kt
@@ -0,0 +1,21 @@
+package com.ozancanguz.moviesapp.data.api
+
+import com.ozancanguz.moviesapp.data.model.searchbyname.SearchByName
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.Query
+
+interface MoviesApi {
+
+    // search by name
+    @Headers(
+        "authorization: apikey 2xAW88zA1iemGAxzWMNumI:47IyS3uICCasjqzWaFtxnn",
+        "content-type: application/json"
+    )
+    @GET("imdbSearchByName")
+    suspend fun searchFilmByName(@Query("query")query: String):Response<SearchByName>
+
+
+
+}
