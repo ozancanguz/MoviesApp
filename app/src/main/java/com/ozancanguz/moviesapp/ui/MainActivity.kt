7d9353fe8baeecda8d98b149ac3ce3229f0efa31
Ozan <ozancangu24@gmail.com>
2023-01-21 17:01:50 +0300

    -initial commit

---
+++ app/src/main/java/com/ozancanguz/moviesapp/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ozancanguz.moviesapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ozancanguz.moviesapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
