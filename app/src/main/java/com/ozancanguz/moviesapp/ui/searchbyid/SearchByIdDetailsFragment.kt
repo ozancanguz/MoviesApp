a259be4f014cc32c41a59513dc9fa89e9670ce49
Ozan <ozancangu24@gmail.com>
2023-01-21 23:13:15 +0300

    -search film by id details screen designed

--- app/src/main/java/com/ozancanguz/moviesapp/ui/searchbyid/SearchByIdDetailsFragment.kt
+++ app/src/main/java/com/ozancanguz/moviesapp/ui/searchbyid/SearchByIdDetailsFragment.kt
@@ -6,17 +6,30 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.ozancanguz.moviesapp.R
+import com.ozancanguz.moviesapp.databinding.FragmentSearchByIdDetailsBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class SearchByIdDetailsFragment : Fragment() {
 
+    private var _binding: FragmentSearchByIdDetailsBinding? = null
+
+    private val binding get() = _binding!!
+
+
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search_by_id_details, container, false)
+
+        _binding = FragmentSearchByIdDetailsBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+
+        return view
+
     }
 
 
