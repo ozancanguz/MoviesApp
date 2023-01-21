68ff6c6d5c63891b18ec0476f9733b1ae0f3a4df
Ozan <ozancangu24@gmail.com>
2023-01-21 17:51:04 +0300

    -search film by name screen designed

--- app/src/main/java/com/ozancanguz/moviesapp/ui/searchbyname/SearchByNameFragment.kt
+++ app/src/main/java/com/ozancanguz/moviesapp/ui/searchbyname/SearchByNameFragment.kt
@@ -5,18 +5,39 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.ozancanguz.moviesapp.R
+import com.ozancanguz.moviesapp.databinding.FragmentSearchByNameBinding
+import com.ozancanguz.moviesapp.viewmodels.SearchByNameViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class SearchByNameFragment : Fragment() {
 
+    private var _binding: FragmentSearchByNameBinding? = null
+
+    private val binding get() = _binding!!
+
+    private val searchByNameViewModel:SearchByNameViewModel by viewModels()
+
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search_by_name, container, false)
+        _binding = FragmentSearchByNameBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        // observe live data and update ui
+        observeLiveData()
+
+
+        return view
+    }
+
+    private fun observeLiveData() {
+
     }
 
 
