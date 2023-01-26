3143b3ed4bf7433c64d3158647de96e2309eb7af
Ozan <ozancangu24@gmail.com>
2023-01-26 20:35:20 +0300

    -favorites adapter created - favorite fragment designed

---
+++ app/src/main/java/com/ozancanguz/moviesapp/data/adapters/FavoritesAdapter.kt
@@ -0,0 +1,44 @@
+package com.ozancanguz.moviesapp.data.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.ozancanguz.moviesapp.R
+import com.ozancanguz.moviesapp.data.db.FavoritesEntity
+import com.ozancanguz.moviesapp.utils.Util.Companion.loadImage
+import kotlinx.android.synthetic.main.searchbyid_fav_row_layout.view.*
+
+class FavoritesAdapter:RecyclerView.Adapter<FavoritesAdapter.FavoriteViewHolder>() {
+    class FavoriteViewHolder(view: View):RecyclerView.ViewHolder(view) {
+
+    }
+
+
+    var favoritesList= emptyList<FavoritesEntity>()
+
+    fun setData(newData:List<FavoritesEntity>){
+        this.favoritesList=newData
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FavoriteViewHolder {
+        val inflater= LayoutInflater.from(parent.context)
+        val view=inflater.inflate(R.layout.searchbyid_fav_row_layout,parent,false)
+        return FavoriteViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: FavoriteViewHolder, position: Int) {
+     val currentFavoritesEntity=favoritesList[position]
+        holder.itemView.searchbyidfavimage.loadImage(currentFavoritesEntity.movies.poster)
+        holder.itemView.searchbyidfavtitle.text=currentFavoritesEntity.movies.title
+        holder.itemView.searchbyidfavyear.text=currentFavoritesEntity.movies.year
+
+
+    }
+
+    override fun getItemCount(): Int {
+       return favoritesList.size
+    }
+
+
+}
