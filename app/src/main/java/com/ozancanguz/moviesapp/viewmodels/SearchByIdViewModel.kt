682e9c5e77718d6a4092a60c2ac9d9e08b086ffe
Ozan <ozancangu24@gmail.com>
2023-01-26 19:57:19 +0300

    -viewmodel favorite functions created

--- app/src/main/java/com/ozancanguz/moviesapp/viewmodels/SearchByIdViewModel.kt
+++ app/src/main/java/com/ozancanguz/moviesapp/viewmodels/SearchByIdViewModel.kt
@@ -2,9 +2,8 @@ package com.ozancanguz.moviesapp.viewmodels
 
 import android.app.Application
 import android.util.Log
-import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.*
+import com.ozancanguz.moviesapp.data.db.FavoritesEntity
 import com.ozancanguz.moviesapp.data.model.searchbyid.SearchByIdModel
 import com.ozancanguz.moviesapp.data.repository.Repository
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -15,9 +14,24 @@ import javax.inject.Inject
 @HiltViewModel
 class SearchByIdViewModel @Inject constructor(private val repository: Repository,application: Application):AndroidViewModel(application){
 
+    // retrofit
     var searchByIdList=MutableLiveData<SearchByIdModel>()
 
 
+    // room
+    var favProductList: LiveData<List<FavoritesEntity>> = repository.local.listAllFavoritesProducts().asLiveData()
+
+
+    // for room insert fav call
+    fun insertFavoriteProduct(favoritesEntity: FavoritesEntity){
+        viewModelScope.launch {
+            repository.local.insertFavorites(favoritesEntity)
+        }
+    }
+
+
+
+    // retrofit function
     fun searchById(movieId:String){
         viewModelScope.launch {
 
