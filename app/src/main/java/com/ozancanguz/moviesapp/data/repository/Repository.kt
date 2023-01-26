dc850aecc8c225033099869db1340fe499474a89
Ozan <ozancangu24@gmail.com>
2023-01-26 19:52:29 +0300

    -repository modified for local data source

--- app/src/main/java/com/ozancanguz/moviesapp/data/repository/Repository.kt
+++ app/src/main/java/com/ozancanguz/moviesapp/data/repository/Repository.kt
@@ -1,11 +1,14 @@
 package com.ozancanguz.moviesapp.data.repository
 
+import com.ozancanguz.moviesapp.data.db.localdatasource.LocalDataSource
 import com.ozancanguz.moviesapp.data.remotedatasource.RemoteDataSource
 import dagger.hilt.android.scopes.ViewModelScoped
 import javax.inject.Inject
 
 @ViewModelScoped
-class Repository@Inject constructor(private val remoteDataSource: RemoteDataSource) {
+class Repository@Inject constructor(private val remoteDataSource: RemoteDataSource,
+localDataSource: LocalDataSource) {
 
     val remote=remoteDataSource
+    var local=localDataSource
 }
